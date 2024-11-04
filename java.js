408f120f61c89b49a1468c932a759976cf869a4b
Rodrigo Gerçóssimo Mattos <133182774+Digoliro11@users.noreply.github.com>
2024-11-04 14:02:39 +0000

    ta osso

---
+++ java.js
@@ -0,0 +1,85 @@
+function mascara_telefone ()
+        {
+           //limitador
+         var tel = document.getElementById("telefone").value
+            console.log(tel)
+          tel=tel.slice(0,14) //(pode limitar a quantidade de char na entrada pelo java script)
+            console.log(tel)
+          document.getElementById("telefone").value=tel
+     tel=document.getElementById("telefone").value.slice(0,10)
+            console.log(tel)
+           
+            //máscara
+            var tel_formatado = document.getElementById("telefone").value
+            if (tel_formatado[0]!="(")
+            {
+                if(tel_formatado[0]!=undefined)
+                {
+                    document.getElementById("telefone").value="("+tel_formatado[0];
+                }
+            }
+
+            if (tel_formatado[3]!=")")
+            {
+                if(tel_formatado[3]!=undefined)
+                {
+                    document.getElementById("telefone").value=tel_formatado.slice(0,3)+")"+tel_formatado[3]
+                }
+            }
+
+            if (tel_formatado[9]!="-")
+            {
+                if(tel_formatado[9]!=undefined)
+                {
+                    document.getElementById("telefone").value=tel_formatado.slice(0,9)+"-"+tel_formatado[9]
+                }
+            }
+        }
+
+        function mascara_cpf()
+        { var cpf_formatado = document.getElementById("cpf").value
+            if (cpf_formatado[4]!=".")
+            {
+                if (cpf_formatado[4]!=undefined)
+                {
+                    document.getElementById("cpf").value=cpf_formatado.slice(0,3)+"."+cpf_formatado[3];
+                }
+            }
+        
+            if (cpf_formatado[6]!="-")
+            {
+                if(cpf_formatado[6]!=undefined)
+                {
+                    document.getElementById("cpf").value=cpf_formatado.slice(0,7)+"."+cpf_formatado[7]
+                }
+            }
+
+            if (cpf_formatado[6]!="-")
+                {
+                    if(cpf_formatado[6]!=undefined)
+                    {
+                        document.getElementById("cpf").value=cpf_formatado.slice(0,7)+"."+cpf_formatado[7]
+                    }
+                }
+
+        } 
+
+        function mascara_cep()
+        { var cep_formatado = document.getElementById("cep").value
+            if (cep_formatado[2]!=".")
+            {
+                if (cep_formatado[2]!=undefined)
+                {
+                    document.getElementById("cep").value=cep_formatado.slice(0,2)+"."+cep_formatado[2];
+                }
+            }
+        
+            if (cep_formatado[6]!="-")
+            {
+                if(cep_formatado[6]!=undefined)
+                {
+                    document.getElementById("cep").value=cep_formatado.slice(0,6)+"-"+cep_formatado[6]
+                }
+            }
+
+        }       
