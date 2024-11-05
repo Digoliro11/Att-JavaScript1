0078b83f3cdddda2b669e875708f6ae668fbb1ba
Rodrigo Gerçóssimo Mattos <133182774+Digoliro11@users.noreply.github.com>
2024-11-05 13:49:32 +0000

    mascaras prontas

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
 function mascara_telefone ()
         {
-           //limitador
+           
          var tel = document.getElementById("telefone").value
             console.log(tel)
-          tel=tel.slice(0,14) //(pode limitar a quantidade de char na entrada pelo java script)
+            tel=tel.slice(0,14) //(pode limitar a quantidade de char na entrada pelo java script)
             console.log(tel)
-          document.getElementById("telefone").value=tel
-     tel=document.getElementById("telefone").value.slice(0,10)
+            document.getElementById("telefone").value=tel
+            tel=document.getElementById("telefone").value.slice(0,10)
             console.log(tel)
            
             //máscara
@@ -36,35 +36,34 @@ function mascara_telefone ()
             }
         }
 
-        function mascara_cpf()
-        { var cpf_formatado = document.getElementById("cpf").value
-            if (cpf_formatado[4]!=".")
+function mascara_cpf()
+        { 
+            var cpf_formatado = document.getElementById("cpf").value
+            if (cpf_formatado[3]!=".")
             {
-                if (cpf_formatado[4]!=undefined)
+                if (cpf_formatado[3]!=undefined)
                 {
                     document.getElementById("cpf").value=cpf_formatado.slice(0,3)+"."+cpf_formatado[3];
                 }
             }
-        
-            if (cpf_formatado[6]!="-")
-            {
-                if(cpf_formatado[6]!=undefined)
+            if (cpf_formatado[7]!=".")
                 {
-                    document.getElementById("cpf").value=cpf_formatado.slice(0,7)+"."+cpf_formatado[7]
-                }
-            }
-
-            if (cpf_formatado[6]!="-")
-                {
-                    if(cpf_formatado[6]!=undefined)
+                    if (cpf_formatado[7]!=undefined)
                     {
-                        document.getElementById("cpf").value=cpf_formatado.slice(0,7)+"."+cpf_formatado[7]
+                        document.getElementById("cpf").value=cpf_formatado.slice(0,7)+"."+cpf_formatado[7];
+                    }
+                }
+            if (cpf_formatado[11]!="-")
+                {
+                    if (cpf_formatado[11]!=undefined)
+                    {
+                        document.getElementById("cpf").value=cpf_formatado.slice(0,11)+"-"+cpf_formatado[11];
                     }
                 }
 
         } 
 
-        function mascara_cep()
+function mascara_cep()
         { var cep_formatado = document.getElementById("cep").value
             if (cep_formatado[2]!=".")
             {
@@ -82,4 +81,85 @@ function mascara_telefone ()
                 }
             }
 
-        }       
+        }    
+        
+function mascara_cnpj()
+        { var cnpj_formatado = document.getElementById("cnpj").value
+            if (cnpj_formatado[2]!=".")
+            {
+                if (cnpj_formatado[2]!=undefined)
+                {
+                    document.getElementById("cnpj").value=cnpj_formatado.slice(0,2)+"."+cnpj_formatado[2];
+                }
+            }
+
+            if (cnpj_formatado[6]!=".")
+                {
+                    if (cnpj_formatado[6]!=undefined)
+                    {
+                        document.getElementById("cnpj").value=cnpj_formatado.slice(0,6)+"."+cnpj_formatado[6];
+                    }
+                }
+
+            if (cnpj_formatado[10]!="/")
+                {
+                    if (cnpj_formatado[10]!=undefined)
+                    {
+                        document.getElementById("cnpj").value=cnpj_formatado.slice(0,10)+"/0001-"+cnpj_formatado[10];
+                    }
+                }
+
+            if (cnpj_formatado[11]!="0")
+                {
+                    if (cnpj_formatado[11]!=undefined)
+                    {
+                        document.getElementById("cnpj").value=cnpj_formatado.slice(0,11)+"0001-"+cnpj_formatado[11];
+                    }
+                }
+        }
+
+
+function mascara_codigo()
+            { var codigo_formatado = document.getElementById("codigo").value
+                if (codigo_formatado[2]!="_")
+                {
+                    if (codigo_formatado[2]!=undefined)
+                    {
+                        document.getElementById("codigo").value=codigo_formatado.slice(0,2)+"_"+codigo_formatado[2];
+                    }
+                }
+
+                if (codigo_formatado[5]!="/")
+                    {
+                        if (codigo_formatado[5]!=undefined)
+                        {
+                            document.getElementById("codigo").value=codigo_formatado.slice(0,5)+"//"+codigo_formatado[5];
+                        }
+                    }
+
+                    if (codigo_formatado[10]!="-")
+                        {
+                            if (codigo_formatado[10]!=undefined)
+                            {
+                                document.getElementById("codigo").value=codigo_formatado.slice(0,10)+"-"+codigo_formatado[10];
+                            }
+                        }
+
+                        if (codigo_formatado[14]!=".")
+                            {
+                                if (codigo_formatado[14]!=undefined)
+                                {
+                                    document.getElementById("codigo").value=codigo_formatado.slice(0,14)+".9"+codigo_formatado[14];
+                                }
+                            }
+
+                            if (codigo_formatado[16]!=".")
+                                {
+                                    if (codigo_formatado[16]!=undefined)
+                                    {
+                                        document.getElementById("codigo").value=codigo_formatado.slice(0,16)+"."+codigo_formatado[16];
+                                    }
+                                }
+                                
+            }
+
